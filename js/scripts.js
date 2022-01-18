0e804c8e7bcd4fcc26c26cdb16d2844d9dd2ae71
jnkiarie <92355202+jnkiarie@users.noreply.github.com>
2022-01-18 17:31:44 +0300

    add jquerry to handle the variable on the input

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,17 @@
+//business logic
 function Contact(first, last) {
     this.firstName = first;
     this.lastName = last;
   }
-  
+
+ // userinterface logic
+
+ $(document).ready(function(){
+     $('form#new-contact').submit(function(event){
+         event.preventDefault();
+         let inputtedfirstName = $('input#new-first-name').val();
+         let inputtedLastName = $('input#new-last-name').val();
+
+
+     })
+ })
